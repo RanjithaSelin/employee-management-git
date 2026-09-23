7251e3daf1e0e78002f61ca4eea78fe8c5961a25
Ranjitha S <Ranjitha.S@infogain.com>
2026-09-23 19:32:00 +0530

    Change employee title on main

--- Prac/main.go
+++ Prac/main.go
@@ -16,7 +16,7 @@ func main() {
 		{ID: 3, Name: "Sam", Salary: 55000},
 	}
 
-	fmt.Println("Employee Report:")
+	fmt.Println("Employee List:")
 
 	for _, employee := range employees {
 		fmt.Println(employee)
