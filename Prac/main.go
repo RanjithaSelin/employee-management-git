402a60348b39db5602ed514831d4f0ed04de8143
Ranjitha S <Ranjitha.S@infogain.com>
2026-09-23 19:32:59 +0530

    Change report title

--- Prac/main.go
+++ Prac/main.go
@@ -16,7 +16,7 @@ func main() {
 		{ID: 3, Name: "Sam", Salary: 55000},
 	}
 
-	fmt.Println("Employee Report:")
+	fmt.Println("Monthly Employee Report:")
 
 	for _, employee := range employees {
 		fmt.Println(employee)
